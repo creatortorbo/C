b5bd4d52da3b06acd4ca3c9ab8a790dd90b19961
creater toro <bablupandey446@gmil.com>
2024-07-10 08:25:47 +0530

    data types

---
+++ variable/data_type.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main (){
+ 
+
+    int a = 3;
+    printf("The printing the integer data value %d\n",a);
+    float b = 8.9;
+    printf("The printing the float value %d\n",b);
+    char c = 'Q';
+    printf("The printing the value of the charter value %d\n",c);
+    return 0;
+}     
